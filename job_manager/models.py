c5ad9c488d06de5b63bd59e6df8a21abc2e702d3
Andrew Wessels <shixish@gmail.com>
2011-11-10 16:53:09 -1000

    Changed the job manager over to the javascript version. Fixed the file upload to use ModelForm which automatically uploads the file and creates the database record.

--- job_manager/models.py
+++ job_manager/models.py
@@ -1,15 +1,19 @@
 from django.db import models
+from django import forms 
+
 import json
 
 class Job(models.Model):
-    date = models.DateTimeField('date published')
+    date_published = models.DateTimeField('date published', auto_now_add=True)
+    date_updated = models.DateTimeField('date updated', auto_now=True)
+    status = models.IntegerField(editable=False, default=0)
     title = models.CharField(max_length = 100)
-    status = models.IntegerField()
-    output = models.CharField(max_length = 100)
-    
-    #def toJSON():
-    #    
+    output = models.CharField(editable=False, max_length = 100)
+    file = models.FileField(upload_to="JOB_UPLOADS")
     
     def __unicode__(self):
-        #return json.dumps([self.title, self.date, self.status, self.output])
         return self.title
+    
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Job
--- job_manager/views.py
+++ job_manager/views.py
@@ -13,22 +13,19 @@ from cloudport.job_manager.models import * #lets me use models...
 
 from django.core import serializers
 def req_data(request):
-    #import django
-    
-    from datetime import date
-    import time
+    #from datetime import date
+    #import time
     #job1 = Job(date=date.today(), title="Test 1", status=0)
     #job1.save()
+    
     import json
     jobs = Job.objects.all()
-    
     #return HttpResponse(jobs)
     return HttpResponse(serializers.serialize('json', jobs))
     #return HttpResponse(repr(request))
-    #return HttpResponse(jobs)
 
-def testing(request):
-    return render_to_response('test.html', {'bootstrap':"TODO"})
+#def testing(request):
+#    return render_to_response('test.html', {'bootstrap':"TODO"})
 
 @login_required()
 def success(request, title):
@@ -37,49 +34,40 @@ def success(request, title):
 @login_required()
 def upload_file(request):
     if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
+        form = JobForm(request.POST, request.FILES)
         if form.is_valid():
-            #p = Person(title=form.cleaned_data['title'], gender="M")
-            handle_uploaded_file(request.FILES['file'])
-            #return HttpResponse(repr(request));
+            form.save()
             return HttpResponseRedirect('/manager/success/')#%request.POST['title'])
-    form = UploadFileForm()
+    form = JobForm()
     c = RequestContext(request)
     c.update(csrf(request))
-    return render_to_response('job_manager/upload.html', {'form': form}, c)
+    return render_to_response('job_manager/jobform.html', {'form': form}, c)
 
-#cant use @login_required because this doesn't take a request variable... could change that.
-def handle_uploaded_file(f):
-    destination = open('/home/jobs_dispatcher/jobs_uploads/'+f.name, 'wb+')
-    for chunk in f.chunks():
-        destination.write(chunk)
-    destination.close()
-
-def get_job_list():
-    items = [
-        {"id":0, "done": False, "name":"Bob job"},
-        {"id":1, "done": False, "name":"Bill job"},
-        {"id":2, "done": False, "name":"job 345"},
-        {"id":3, "done": True, "name":"Jill job", "output":"infosthetics02.jpg"},
-        {"id":4, "done": True, "name":"Crazy cool job", "output":"graphs.jpg"},
-    ]
-    dir = "/home/jobs_dispatcher/jobs_finished/"
-    filelist = os.listdir(dir)
-    filelist = filter(lambda x: not os.path.isdir(dir+x), filelist)
-    newest = max(filelist, key=lambda x: os.stat(dir+x).st_mtime)
-    items.append({"id":5, "done": True, "name":"Latest Result", "output":newest})
-    return items
-
-#from django.core import serializers
+#def get_job_list():
+#    items = [
+#        {"id":0, "done": False, "name":"Bob job"},
+#        {"id":1, "done": False, "name":"Bill job"},
+#        {"id":2, "done": False, "name":"job 345"},
+#        {"id":3, "done": True, "name":"Jill job", "output":"infosthetics02.jpg"},
+#        {"id":4, "done": True, "name":"Crazy cool job", "output":"graphs.jpg"},
+#    ]
+#    dir = "/home/jobs_dispatcher/jobs_finished/"
+#    filelist = os.listdir(dir)
+#    filelist = filter(lambda x: not os.path.isdir(dir+x), filelist)
+#    newest = max(filelist, key=lambda x: os.stat(dir+x).st_mtime)
+#    items.append({"id":5, "done": True, "name":"Latest Result", "output":newest})
+#    return items
+#from django.core import serializers
 #import json
 #@login_required()
-def get(request, thing):
-    if (thing == 'jobs'):
-        #return HttpResponse(serializers.serialize('json', Job.objects.all()))
-        return HttpResponse(json.dumps(get_job_list()))
+#def get(request, thing):
+#    if (thing == 'jobs'):
+#        #return HttpResponse(serializers.serialize('json', Job.objects.all()))
+#        return HttpResponse(json.dumps(get_job_list()))
 
 @login_required()
 def manager(request):
-    items = get_job_list()
-    return render_to_response('job_manager/manager.html', {"items":items}, context_instance=RequestContext(request))
+    #items = get_job_list()
+    #return render_to_response('job_manager/manager.html', {"items":items}, context_instance=RequestContext(request))
+    return render_to_response('test.html', {'bootstrap':"TODO"})
 
